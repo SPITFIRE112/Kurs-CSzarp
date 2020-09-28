559be1895a3b55321655a0ef4f6568b2fbc0a4c4
Cypis <cyprian.napewnoniehenzel@ont.pl>
2020-09-28 19:25:59 +0200

    zajecia 2

---
+++ test1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz tekst");
+            string tekst = Console.ReadLine();
+            tekst = tekst.Replace("a","@");
+            Console.WriteLine(tekst);
+            Console.ReadKey();
+        }
+    }
+}
--- test1/Program.cs
+++ test1/Program.cs
@@ -10,20 +10,35 @@ namespace test1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Podaj liczbę");
-            int numer = Int32.Parse(Console.ReadLine());
 
-
-            if (numer <= 50)
+            try
             {
-                Console.WriteLine("Tak! Liczba jest mniejsza lub równa 50");
-            }
-            else
-            {
-                Console.WriteLine("Nie! Liczba jest większa od 50");
+                
+                int numer = Int32.Parse(Console.ReadLine());
+
+
+                if (numer <= 50)
+                {
+                    Console.WriteLine("Tak! Liczba jest mniejsza lub równa 50");
+                }
+                else
+                {
+                    Console.WriteLine("Nie! Liczba jest większa od 50");
+                }
+                Console.ReadLine();
             }
 
-            Console.ReadLine();
+            catch
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Wpisana wartość musi być cyfrą!");
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Bla;
+                Main(args);
+            }
+
+           
             Main(args);
         }
     }
